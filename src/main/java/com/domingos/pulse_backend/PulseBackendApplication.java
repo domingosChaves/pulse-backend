712327cd4eaf86093ae4337a45b0932433f42ec9
Domingos Chaves <dudu.dekst18@gmail.com>
2025-09-02 20:15:40 -0300

    feat: Criar projeto base em Spring Boot, configurar dependências e banco H2 em memória para suportar o desenvolvimento dos microserviços de cadastro de fabricantes e produtos.
    Issue: #1
    Link: https://github.com/domingosChaves/pulse-backend/issues/1

---
+++ src/main/java/com/domingos/pulse_backend/PulseBackendApplication.java
@@ -0,0 +1,13 @@
+package com.domingos.pulse_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PulseBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PulseBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/domingos/pulse_backend/PulseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.domingos.pulse_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PulseBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
