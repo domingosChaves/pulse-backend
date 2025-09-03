f16a633f4d33a6f659a939b16d3f7a182d8e47aa
Domingos Chaves <dudu.dekst18@gmail.com>
2025-09-02 22:46:32 -0300

    feat: adicionar documentacao OpenAPI (Swagger) e colecao Postman\n\nIssue: #7\nLink: https://github.com/domingosChaves/pulse-backend/issues/7

---
+++ src/main/java/com/domingos/pulse_backend/config/OpenApiConfig.java
@@ -0,0 +1,29 @@
+package com.domingos.pulse_backend.config;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Pulse Backend API")
+                        .version("v1")
+                        .description("API de exemplo para gerenciar Fabricantes e Produtos")
+                        .contact(new Contact().name("Equipe de Desenvolvimento").email("dev@pulse.local"))
+                        .license(new License().name("MIT").url("https://opensource.org/licenses/MIT"))
+                )
+                .externalDocs(new ExternalDocumentation()
+                        .description("Repositorio do projeto")
+                        .url("https://github.com/domingosChaves/pulse-backend"));
+    }
+}
+
